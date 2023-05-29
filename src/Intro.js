4d706fac57ad00777ccaa1c41fb531b21e0e2d3b
yuzuki1027 <yuzuki1027@gmail.com>
2023-05-29 16:18:09 +0900

    ついか

--- src/Intro.js
+++ src/Intro.js
@@ -1,19 +1,18 @@
 
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
 
 function Intro() {
   return (
     <div className="intro_back">
       <div class="container">
   <div class="row">
-    <div class="col-sm-6 bg-black ">
-      aaaa
+    <div class="col-sm-6 bg-black mt-1 mb-2 intro-left">
+      <h3>ご挨拶</h3>
+      <p >高本電業株式会社のホームページにお越し頂きありがとう御座います。</p>
+      <p >高本電業では電力会社の移動作業業務委託、電力会社の電気工事調整委託を行っています。</p>
     </div>
     <div class="col-sm-6 intro_right_bg">
-     aaa
     </div>
   </div>
 </div>
